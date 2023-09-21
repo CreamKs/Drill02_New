d5705dd355dbc3e06a6fa4a7839ef3caaf582f1a
신창화 <sch9270@gmail.com>
2023-09-21 12:20:39 +0900

    그리기 함수 추가

--- Drill02.py
+++ Drill02.py
@@ -6,27 +6,26 @@ open_canvas()
 character = load_image('character.png')
 grass = load_image('grass.png')
 
-grass.draw_now(400, 30)
-character.draw_now(400, 90)
-delay(1)
+def rander_frame(x, y):
+		clear_canvas_now()
+		grass.draw_now(400, 30)
+		character.draw_now(x, y)
+		delay(0.1)
+
+rander_frame(400, 90)
 
 def run_circle():
 	cx, cy, r = 400, 300, 200
 	for deg in range(0, 360, 5):
 		x = cx + r * math.cos(math.radians(deg))
 		y = cy + r * math.sin(math.radians(deg))
-		clear_canvas_now()
-		grass.draw_now(400, 30)
-		character.draw_now(x, y)
-		delay(0.1)
+		rander_frame(x, y)
+
 
 def run_rectangle():
 	for x in range(50, 750+1, 5):
-		clear_canvas_now()
-		grass.draw_now(400, 30)
-		character.draw_now(x, 90)
-		delay(0.1)
 		
+
 
 
 while True:
