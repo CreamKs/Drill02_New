9192c8d6324ee345b714bb673a8c1b0a3cfdb9a8
신창화 <sch9270@gmail.com>
2023-09-21 12:32:26 +0900

    오른쪽 상승 완료

--- Drill02.py
+++ Drill02.py
@@ -27,9 +27,13 @@ def run_rectangle():
 	#for x in range(50, 750 + 1, 5):
 	#	rander_frame(x, 90)
 
+	#right line
+	for y in range(90, 550 + 1, 5):
+		rander_frame(750, y)
+
 	#top line
-	for x in range(750, 50 - 1, -5):
-		rander_frame(x, 550)
+#	for x in range(750, 50 - 1, -5):
+#		rander_frame(x, 550)
 
 
 
