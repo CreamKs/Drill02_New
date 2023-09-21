c1332f59f8917b629e2177e1f8368a8baf9a4cc8
신창화 <sch9270@gmail.com>
2023-09-21 11:43:51 +0900

    Draw

--- Drill02.py
+++ Drill02.py
@@ -3,5 +3,11 @@ import math
 
 open_canvas()
 
+character = load_image('character.png')
+grass = load_image('grass.png')
+
+grass.draw_now(400, 30)
+character.draw_now(400, 90)
+delay(1)
 
 close_canvas()
